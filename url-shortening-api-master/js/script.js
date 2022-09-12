85e00f3b2f637f1091a3b78e2675c84e274b7e5d
mohamed khaled <mk6601152@gmail.com>
2022-09-12 15:36:20 +0200

    files added

---
+++ url-shortening-api-master/js/script.js
@@ -0,0 +1,7 @@
+// start code for list 
+let bar = document.querySelector('.bar');
+let navbar = document.querySelector('.navbar');
+bar.addEventListener('click',function(){
+    navbar.classList.toggle('active')
+});
+// end code for list
